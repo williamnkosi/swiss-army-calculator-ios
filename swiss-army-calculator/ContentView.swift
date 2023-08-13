8dba83ea5aebf9301185285246cb5707ace588fc
William Nkosi <william.dev.acc@gmail.com>
2023-08-13 01:28:36 -0400

    initial project setup

---
+++ swiss-army-calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiss-army-calculator
+//
+//  Created by William Nkosi on 8/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiss-army-calculator/swiss_army_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiss_army_calculatorApp.swift
+//  swiss-army-calculator
+//
+//  Created by William Nkosi on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct swiss_army_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
