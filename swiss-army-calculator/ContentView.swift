5d2740452d131df5902dc14e8f75ee253300a0ce
William Nkosi <william.dev.acc@gmail.com>
2023-08-15 18:41:25 -0400

    Setup initial bottom navigation tab
    Signed-off-by: William Nkosi <william.dev.acc@gmail.com>

--- swiss-army-calculator/ContentView.swift
+++ swiss-army-calculator/ContentView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedTab = 0
+    @State var headerTitle = "Home"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            TabView {
+                NavigationView{
+                    HomeView()
+                }
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                        .tag(0)
+                }
+                NavigationView{
+                    FavoritesView()
+                }.tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Home")
+                        .tag(1)
+                }
+                NavigationView{
+                    SettingsView()
+                }
+               .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Home")
+                        .tag(2)
+                }
+            } .accentColor(.green)
+            .background(.red)
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ swiss-army-calculator/views/FavoritesView.swift
@@ -0,0 +1,22 @@
+//
+//  FavoritesView.swift
+//  swiss-army-calculator
+//
+//  Created by William Nkosi on 8/13/23.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        List {
+            
+        }.navigationTitle("Favorites")
+    }
+}
+
+struct FavoritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesView()
+    }
+}
