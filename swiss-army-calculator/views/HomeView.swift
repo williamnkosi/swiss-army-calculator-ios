329a918ba7ef8c1fd89733fb680c586d14f46037
William Nkosi <william.dev.acc@gmail.com>
2023-08-28 17:58:56 -0400

    Added functinality to make the navigation title disappear when the search filed is in focus

--- swiss-army-calculator/views/HomeView.swift
+++ swiss-army-calculator/views/HomeView.swift
@@ -9,23 +9,22 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var searchText = ""
+    @State var isTyping: Bool = false
     var body: some View {
         ScrollView(showsIndicators: false) {
             HStack {
                 HStack {
                     Image(systemName: "magnifyingglass")
-                    TextField("Search", text: $searchText)
+                    TextField("Search", text: $searchText) {
+                        self.isTyping = $0
+                    }
                         
                 }.padding(6)
                
             }.background(Color(.systemGray5))
             .cornerRadius(5)
             .padding(.bottom,8)
-            
-        
-          
             VStack{
-                
                 CategoryTile(title: "Finance", imageUrl: "dog2", description: "Calculator related to financal health")
                 CategoryTile(title: "Finance1", imageUrl: "dog2", description: "Calculator related to financal health")
                 CategoryTile(title: "Finance2", imageUrl: "dog2", description: "Calculator related to financal health")
@@ -33,7 +32,7 @@ struct HomeView: View {
                 CategoryTile(title: "Finance4", imageUrl: "dog2", description: "Calculator related to financal health")
                 CategoryTile(title: "Finance", imageUrl: "dog2", description: "Calculator related to financal health")
             }
-        }.navigationTitle("Home")
+        }.navigationTitle("Home").navigationBarHidden(isTyping ? true : false)
             .padding(.horizontal)
            
     }
